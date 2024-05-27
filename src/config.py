af0db3f9d8f799f9c0414841f47a2dfccd5a9e91
Miguel Figueira Ferraz <miguelfigueiraferraz@gmail.com>
2024-05-27 17:27:39 +0000

    feat: adds basic `Config` class

---
+++ src/config.py
@@ -0,0 +1,10 @@
+from os import getenv
+from dataclasses import dataclass
+
+from dotenv import load_dotenv
+
+@dataclass
+class Config:
+    load_dotenv()
+
+    ADUANA_PY_BASE_URL: str = str(getenv("ADUANA_PY_BASE_URL", ""))
