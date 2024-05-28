ece686bfcb060967127a8b4efec327532342717b
Miguel Figueira Ferraz <miguelfigueiraferraz@gmail.com>
2024-05-28 12:28:08 -0300

    feat: utilizes multithread to fetch the data

---
+++ src/main.py
@@ -0,0 +1,28 @@
+from multiprocessing.pool import ThreadPool
+
+from src.aduana_py_client import AduanaPYClient
+from src.config import Config
+
+
+def main():
+    aduana_client = AduanaPYClient(base_url=Config.ADUANA_PY_BASE_URL)
+
+    pool = ThreadPool(processes=5)
+    results = []
+
+    for i in range(1, 6):
+        async_result = pool.apply_async(
+            aduana_client.fetch_paginated, ("2023", f"310{i}.00.00")
+        )
+
+        results.append(async_result)
+
+    flat_results = []
+    for result in results:
+        flat_results.extend(result.get())
+
+    print(len(results))
+
+
+if __name__ == "__main__":
+    main()
